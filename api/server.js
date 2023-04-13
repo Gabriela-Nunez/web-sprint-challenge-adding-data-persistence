1c398783caa7450c94040b341825c0da3d974ce0
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-13 15:18:02 -0500

    created index.js and server.js.

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+
+const server = express();
+
+server.use(express.json());
+
+
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'not found'})
+})
+
+
+
+module.exports = server;
