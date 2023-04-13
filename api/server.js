9df85e5a262130f0569245b781c711a00dd43e46
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-13 15:32:09 -0500

    connected all routers to server.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 // build your server here and require it from index.js
 const express = require('express');
-
+const resourceRouter = require('../api/resource/router')
+const projectRouter = require('../api/project/router')
+const taskRouter = require('../api/task/router')
 
 const server = express();
 
 server.use(express.json());
-
+server.use('/api/resources', resourceRouter)
+server.use('/api/projects', projectRouter)
+server.use('/api/task', taskRouter)
 
 server.use('*', (req, res, next) => {
   next({ status: 404, message: 'not found'})
