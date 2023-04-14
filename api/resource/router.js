36b6339e121c16ed2cacbfcb6d9a36dfdbecb0f2
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-13 23:04:47 -0500

    resource.model and resource.router are complete.

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,34 @@
 // build your `/api/resources` router here
 const express = require('express')
+const Resource = require('./model')
 const router = express.Router()
+
+
+router.get('/', (req, res, next) => {
+  Resource.getResources()
+    .then(resources => {
+      res.json(resources)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res) => {
+  const resource = req.body;
+
+  if (!resource.resource_name) {
+    res.status(400).json({ message: 'Resource name is required' });
+  } else {
+    Resource.addResource(resource)
+      .then(newResource => {
+        res.status(201).json(newResource);
+      })
+      .catch(error => {
+        res.status(500).json({ 
+          message: 'Error adding resource',
+          error: error.message });
+      });
+  }
+});
 
 router.use((err, req, res, next) => { //eslint-disable-line
   res.status(500).json({
