d466e2c525ba69886b5e8522352cd28a9afb1c20
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-14 12:54:34 -0500

    task model and router files are complete. All test are passing and MVP was met.

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const server = express();
 server.use(express.json());
 server.use('/api/resources', resourceRouter)
 server.use('/api/projects', projectRouter)
-server.use('/api/task', taskRouter)
+server.use('/api/tasks', taskRouter)
 
 server.use('*', (req, res) => {
   res.status(404).json({
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,26 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+function getTasks() {
+  return db('tasks')
+    .join('projects', 'tasks.project_id', 'projects.project_id')
+    .select('tasks.task_id', 
+            'tasks.task_description', 
+            'tasks.task_notes', 
+            'tasks.task_completed', 
+            'projects.project_name', 
+            'projects.project_description');
+}
+
+function addTask(task) {
+  return db('tasks')
+    .insert(task)
+    .then(ids => {
+      return db('tasks').where({ task_id: ids[0] }).first();
+    });
+}
+
+module.exports = {
+  addTask,
+  getTasks,
+};
